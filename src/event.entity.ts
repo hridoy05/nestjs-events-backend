a03e1e4cd3c8b07422e498621b290d8ad70ac03e
Hridoy Banik <hridoy.banik71@gmail.com>
2022-08-12 00:45:55 +0600

    add the typeorm event entity

--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,7 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('event')
 export class Event {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+  @Column()
   description: string;
+  @Column()
   when: Date;
+  @Column()
   address: string;
 }
